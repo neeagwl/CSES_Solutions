a2d2bc5c73ebbe573a1588b7368a6f9878da235e
Neetu Agarwal <agarwalneetu301@gmail.com>
2021-02-01 21:04:43 +0530

    add werid problem

---
+++ intro_problems/weird_algorithm.cpp
@@ -0,0 +1,63 @@
+/********   All Required Header Files ********/
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*******  All Required define Pre-Processors and typedef Constants *******/
+#define MEM(a, b) memset(a, (b), sizeof(a))
+#define all(cont) cont.begin(), cont.end()
+#define rall(cont) cont.end(), cont.begin()
+#define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
+typedef map<int,int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef long int l;
+typedef unsigned long int ul;
+typedef long long int ll;
+typedef unsigned long long int  ull;
+
+int max_no=INT_MAX;
+int min_no=INT_MIN;
+
+void weirdAlgo(ll n){
+    cout<<n<<" ";
+
+    //base case
+    if(n==1){
+        return;
+    }
+    //rec case
+    if(n%2==0){
+        weirdAlgo(n/2);
+    }else{
+        weirdAlgo((3*n+1));
+    }
+}
+
+void solve(){
+    ll n;
+    cin>>n;
+    weirdAlgo(n);
+    cout<<'\n';
+}
+
+/********** Main()  function **********/
+int main()
+{
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL); 
+    solve();
+}
